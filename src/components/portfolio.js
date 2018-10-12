e3876b7846f27479c3c362963354c3902dc92727
Matthew Hoth <matthewhoth@gmail.com>
2018-10-12 14:35:42 +0800

    Rough draft the portfolio left side, adding basic css classes for style

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -5,34 +5,34 @@ import i18n from "i18next";
 export default class Portfolio extends React.PureComponent {
   render() {
     return (
-      <header>
-        <div>
-          <section>
-            <button>
+      <header className="app-portfolio-container">
+        <div className="app-portfolio">
+          <section className="portfolio-resume">
+            <button className="resume-download btn">
               <i />
               <Trans i18nKey="welcome.resume" />
             </button>
             <button onClick={() => i18n.changeLanguage("ch")}>ch</button>
             <button onClick={() => i18n.changeLanguage("en")}>en</button>
           </section>
-          <section>
-            <div>
-              <h1>
-                <span>
+          <section className="portfolio-title-container">
+            <div className="portfolio-title">
+              <h1 className="portfolio-title-top">
+                <span className="portfolio-title-upper">
                   <Trans i18nKey="welcome.title-upper" />
                 </span>
-                <span>
+                <span className="portfolio-title-lower">
                   <Trans i18nKey="welcome.title-lower" />
                 </span>
               </h1>
             </div>
           </section>
-          <nav>
-            <ul>
+          <nav className="portfolio-contact">
+            <ul className="portfolio-contact-list">
               <li>
                 <a href="#" target="_blank">
                   <i />
-                  <span>
+                  <span className="i-label">
                     <Trans i18nKey="welcome.location" />
                   </span>
                 </a>
@@ -40,13 +40,13 @@ export default class Portfolio extends React.PureComponent {
               <li>
                 <a href="#" target="_blank">
                   <i />
-                  <span>/in/ahyeah</span>
+                  <span className="i-label">/in/ahyeah</span>
                 </a>
               </li>
               <li>
                 <a href="#" target="_blank">
                   <i />
-                  <span>
+                  <span className="i-label">
                     <Trans i18nKey="welcome.github" />
                   </span>
                 </a>
@@ -54,13 +54,13 @@ export default class Portfolio extends React.PureComponent {
               <li>
                 <a href="#">
                   <i />
-                  <span>Phone Number</span>
+                  <span className="i-label">Phone Number</span>
                 </a>
               </li>
               <li>
                 <a href="#">
                   <i />
-                  <span>name@myname.com</span>
+                  <span className="i-label">name@myname.com</span>
                 </a>
               </li>
             </ul>
