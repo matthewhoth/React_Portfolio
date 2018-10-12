8d42fc2bd509cc8dd603c9cc645e2c264567a494
Matthew Hoth <matthewhoth@gmail.com>
2018-10-12 16:24:08 +0800

    Use fontawesome icons to decorate the leftside contact panel

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -9,7 +9,7 @@ export default class Portfolio extends React.PureComponent {
         <div className="app-portfolio">
           <section className="portfolio-resume">
             <button className="resume-download btn">
-              <i />
+              <i className="fas fa-download" />
               <Trans i18nKey="welcome.resume" />
             </button>
             <button onClick={() => i18n.changeLanguage("ch")}>ch</button>
@@ -30,8 +30,8 @@ export default class Portfolio extends React.PureComponent {
           <nav className="portfolio-contact">
             <ul className="portfolio-contact-list">
               <li>
-                <a href="#" target="_blank">
-                  <i />
+                <a href="https://goo.gl/maps/6uY34GshTM22" target="_blank">
+                  <i className="fas fa-map-marker-alt" />
                   <span className="i-label">
                     <Trans i18nKey="welcome.location" />
                   </span>
@@ -39,13 +39,13 @@ export default class Portfolio extends React.PureComponent {
               </li>
               <li>
                 <a href="#" target="_blank">
-                  <i />
+                  <i className="fab fa-linkedin-in" />
                   <span className="i-label">/in/ahyeah</span>
                 </a>
               </li>
               <li>
                 <a href="#" target="_blank">
-                  <i />
+                  <i className="fab fa-github" />
                   <span className="i-label">
                     <Trans i18nKey="welcome.github" />
                   </span>
@@ -53,13 +53,13 @@ export default class Portfolio extends React.PureComponent {
               </li>
               <li>
                 <a href="#">
-                  <i />
+                  <i className="fas fa-phone" />
                   <span className="i-label">Phone Number</span>
                 </a>
               </li>
               <li>
                 <a href="#">
-                  <i />
+                  <i className="fas fa-envelope" />
                   <span className="i-label">name@myname.com</span>
                 </a>
               </li>
