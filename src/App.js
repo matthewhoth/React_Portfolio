bd87e1c145ad6eb33971767dbb71de86f9249aa4
Matthew Hoth <matthewhoth@gmail.com>
2018-10-14 20:23:05 +0800

    Clean up the stylesheet imports by grouping them in one single file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { translate } from "react-i18next";
 import Profile from "./components/profile.js";
 import Main from "./components/work/main";
-import "./styles/profile.css";
-import "./styles/portfolio.css";
-import "./styles/portfolio-buttons.css";
-import "./styles/project-portfolio.css";
-import "./styles/fonts.css";
-import "./styles/query.css";
-import "./styles/animate.css";
-import "./styles/bar.css";
-import "./styles/rightside.css";
-import "./styles/sections.css";
-import "./styles/tools.css";
-import "./styles/projects.css";
-import { translate } from "react-i18next";
+import "./styles/index.js";
 
 export const App = () => (
   <BrowserRouter>
