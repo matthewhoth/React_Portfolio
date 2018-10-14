e8f5ba1ab58778467c2c07f9a5dd36de59d58d9b
Matthew Hoth <matthewhoth@gmail.com>
2018-10-14 16:48:18 +0800

    Create the main project page outline

---
+++ src/components/work/main.js
@@ -0,0 +1,20 @@
+import React from "react";
+export default class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <main className="project-portfolio">
+        <div className="wrapper">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+        </div>
+      </main>
+    );
+  }
+}
