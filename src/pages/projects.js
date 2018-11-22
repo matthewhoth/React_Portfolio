71c9f42cb506117987e7c68af9612e7bdfe4b9ce
Matthew Hoth <matthewhoth@gmail.com>
2018-11-22 22:16:19 +0800

    What if I didn't init

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,22 +3,7 @@ import ProjectWrapper from '../projectWrapper'
 import { I18nextProvider } from 'react-i18next'
 import i18next from 'i18next'
 import { Helmet } from 'react-helmet'
-import common_ch from '../translations/ch.json'
-import common_en from '../translations/en.json'
 import logo from '../images/logoxx.ico'
-
-i18next.init({
-  interpolation: { escapeValue: false },
-  lng: 'ch',
-  resources: {
-    en: {
-      common: common_en,
-    },
-    ch: {
-      common: common_ch,
-    },
-  },
-})
 
 const projectpage = () => (
   <I18nextProvider i18n={i18next}>
