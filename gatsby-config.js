b929de185fda2726788d9ca25d9378a5c6ded549
Matthew Hoth <matthewhoth@gmail.com>
2018-12-10 18:59:50 +0800

    Help push the app towards a PWA life, adding a service worker an manifest json

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: `Matthew Hoth's Portoflio`,
+        short_name: `Matthew's Port`,
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
@@ -25,8 +25,6 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    'gatsby-plugin-offline',
   ],
 }
